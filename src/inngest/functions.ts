0a55884838e3cef0b90cbf2391331dab1a91f048
Devawayan <devawayan.hore-extern@renault.com>
2025-09-11 00:58:07 +0530

    AI Jobs

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,15 +1,24 @@
+import { openai, createAgent } from "@inngest/agent-kit";
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
-    //This is an Transcript Step.
-    await step.sleep("wait-a-moment", "10s");
+  async ({ event }) => {
+    if (!event.data.value || event.data.value.trim() === "") {
+      return { output: "No input provided." };
+    }
 
-    //This is an Summary Step.
-    await step.sleep("wait-a-moment", "5s");
+    const codeAgent = createAgent({
+      name: "code-agent",
+      system: "You are an expert next.js developer. You write readable, maintainable code. You write simple Next.js & React snippets.",
+      model: openai({ model: "gpt-4.1" }),
+    });
 
-    return { message: `Hello ${event.data.email}!` };
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.value}`,
+    );
+
+    return { output };
   },
 );
